21fbba999396e089c2bf356602bc77f54595e5ec
Krimsit <KrimsitDjenkis@yandex.ru>
2021-08-16 11:57:44 +0300

    Fix connection to socket server and add helpers

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,13 +2,26 @@ import * as React from "react"
 import { Route, Switch, Redirect } from "react-router-dom"
 import { connect } from "react-redux"
 
+import { socket } from "./core"
+import { getNotification, getData } from "./utils/helper"
+
 import { Authentication, Dashboard } from "./pages"
 
 interface IAppContainerProps {
     isAuth: boolean
+    client_id: string
 }
 
-const App: React.FC<IAppContainerProps> = ({ isAuth }) => {
+const App: React.FC<IAppContainerProps> = ({ isAuth, client_id }) => {
+    React.useEffect(() => {
+        socket.on("connect", async () => {
+            socket.emit("SERVER:JOIN_ROOM", { user: true, client_id: client_id })
+        })
+    }, [])
+
+    getNotification()
+    getData(client_id)
+
     return (
         <div style={{ background: "#e5e5e5", width: "100%", height: "100%" }}>
             <Switch>
@@ -19,8 +32,8 @@ const App: React.FC<IAppContainerProps> = ({ isAuth }) => {
     )
 }
 
-const mapStateToProps = (state: any): { isAuth: boolean } => {
-    return { isAuth: state.user.isAuth }
+const mapStateToProps = (state: any): { isAuth: boolean; client_id: string } => {
+    return { isAuth: state.user.isAuth, client_id: state.user._id }
 }
 
 export default connect(mapStateToProps)(App)

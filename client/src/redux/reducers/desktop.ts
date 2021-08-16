085ebaf4fa1b4987cf8647fccd3dd352e8c6b568
Krimsit <KrimsitDjenkis@yandex.ru>
2021-08-16 11:56:26 +0300

    Add static and dynamic data to desktop reducer

--- client/src/redux/reducers/desktop.ts
+++ client/src/redux/reducers/desktop.ts
@@ -1,27 +1,94 @@
+import { IDesktopData, IDesktopDynamicData, IDesktopStaticData } from "../../interface/desktop"
 interface IDesktopInitialStateProps {
-    _id: string
-    name: string
-    online: boolean
+    _id?: string
+    name?: string
+    hostname?: string
+    online?: boolean
+    static_data?: IDesktopStaticData
+    dynamic_data?: IDesktopDynamicData
 }
 
 interface IActionProps {
     type: string
-    payload: any
+    payload: IDesktopData & IDesktopDynamicData & IDesktopStaticData
 }
 
 const initialState: IDesktopInitialStateProps = {
     _id: null,
     name: null,
     online: false,
+    static_data: {
+        model: null,
+        cpu: {
+            model: null,
+            cores: 0,
+            speed: null,
+        },
+        memory: 0,
+        graphics: [
+            {
+                model: null,
+                vram: null,
+            },
+        ],
+        os: {
+            distro: null,
+            arch: null,
+        },
+        disks: [
+            {
+                model: null,
+                size: 0,
+            },
+        ],
+    },
+    dynamic_data: {
+        memory: [
+            {
+                size: 0,
+                bank: null,
+            },
+        ],
+        battery: {
+            hasBattery: false,
+            isCharging: false,
+        },
+        disks: [
+            {
+                fs: null,
+                type: null,
+                size: 0,
+                used: 0,
+                available: 0,
+                use: 0,
+            },
+        ],
+    },
 }
 
-export default function user(state = initialState, action: IActionProps) {
+export default function user(state = initialState, action: IActionProps): IDesktopInitialStateProps {
     switch (action.type) {
         case "DESKTOP:SET_DESKTOP":
             return {
                 _id: action.payload._id,
                 name: action.payload.name,
                 online: action.payload.online,
+            }
+        case "DESKTOP:SET_DATA":
+            return {
+                ...state,
+                hostname: action.payload.hostname,
+                online: action.payload.online,
+            }
+        case "DESKTOP:SET_STATIC_DATA":
+            return {
+                ...state,
+                static_data: action.payload,
+            }
+        case "DESKTOP:SET_DYNAMIC_DATA":
+            return {
+                ...state,
+                dynamic_data: action.payload,
             }
         default:
             return state

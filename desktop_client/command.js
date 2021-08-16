18817d73365f50d017818ecacc92ab8b2d0371fa
Krimsit <KrimsitDjenkis@yandex.ru>
2021-08-16 11:47:34 +0300

    Added receipt of dynamic information

--- desktop_client/command.js
+++ desktop_client/command.js
@@ -1,29 +1,72 @@
 import si from "systeminformation"
 
 const command = {
-    getPCInfo: async () => {
-        const pc_info = { model: null, baseboard: null, cpu: null, memory: null, graphics: null, os: null, disks: null }
+    getStaticData: async () => {
+        // const data = { model: null, baseboard: null, cpu: null, memory: null, graphics: null, os: null, disks: null }
+
+        const data = {
+            model: null,
+            cpu: {
+                model: null,
+                cores: null,
+                speed: null,
+            },
+            memory: null,
+            graphics: [
+                {
+                    model: null,
+                    vram: null,
+                },
+            ],
+            os: {
+                distro: null,
+                arch: null,
+                hostname: null,
+            },
+            disks: [
+                {
+                    model: null,
+                    size: null,
+                },
+            ],
+        }
 
         const system = await si.system()
-        const baseboard = await si.baseboard()
         const cpu = await si.cpu()
         const mem = await si.mem()
         const graphics = await si.graphics()
         const osInfo = await si.osInfo()
         const diskLayout = await si.diskLayout()
-        pc_info.model = system.model
-        pc_info.baseboard = `${baseboard.manufacturer}${baseboard.model}`
-        pc_info.cpu = { model: `${cpu.manufacturer} ${cpu.brand}`, cores: cpu.physicalCores, speed: `${cpu.speed} ГГц` }
-        pc_info.memory = mem.total
-        pc_info.graphics = graphics.controllers.map((controller) => {
+
+        data.model = system.model
+        data.cpu = { model: `${cpu.manufacturer} ${cpu.brand}`, cores: cpu.physicalCores, speed: `${cpu.speed} ГГц` }
+        data.memory = mem.total
+        data.graphics = graphics.controllers.map((controller) => {
             return { model: controller.model, vram: controller.vram }
         })
-        pc_info.os = { distro: osInfo.distro, arch: osInfo.arch, hostname: osInfo.hostname }
-        pc_info.disks = diskLayout.map((disk) => {
+        data.os = { distro: osInfo.distro, arch: osInfo.arch, hostname: osInfo.hostname }
+        data.disks = diskLayout.map((disk) => {
             return { model: disk.name, size: disk.size }
         })
 
-        return pc_info
+        return data
+    },
+    getDynamicData: async () => {
+        const data = {
+            memory: null,
+            battery: null,
+            disks: null,
+        }
+
+        const memory = await si.memLayout()
+        const battery = await si.battery()
+        const disks = await si.fsSize()
+
+        data.memory = memory
+        data.battery = battery
+        data.disks = disks
+
+        return data
     },
 }
 

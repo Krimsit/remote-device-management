e26869121aee8c5f3702c8af7106c2692f6cae2e
Krimsit <KrimsitDjenkis@yandex.ru>
2021-08-16 11:48:02 +0300

    Added sending information

--- desktop_client/app.js
+++ desktop_client/app.js
@@ -15,12 +15,24 @@ const http = Server()
 const server = io(process.env.SOCKET_SERVER_URL)
 
 server.on("connect", async () => {
-    server.emit("SERVER:JOIN_ROOM", { pc: true, client_id: process.env.CLIENT_ID, information: await command.getPCInfo() })
+    server.emit("SERVER:JOIN_ROOM", { pc: true, client_id: process.env.CLIENT_ID, static_data: await command.getStaticData(), dynamic_data: await command.getDynamicData() })
 })
+
+server.on("PC:GET_STATIC_DATA", async () => {
+    server.emit("PC:STATIC_DATA", { pc: true, client_id: process.env.CLIENT_ID, static_data: await command.getStaticData() })
+})
+
+server.on("PC:GET_DYNAMIC_DATA", async () => {
+    server.emit("PC:DYNAMIC_DATA", { pc: true, client_id: process.env.CLIENT_ID, dynamic_data: await command.getDynamicData() })
+})
+
+setInterval(async () => {
+    server.emit("PC:DYNAMIC_DATA", { pc: true, client_id: process.env.CLIENT_ID, dynamic_data: await command.getDynamicData() })
+}, 300000)
 
 process.stdin.setEncoding("utf8")
 process.stdin.on("keypress", (ch, key) => {
-    if (key.name === "c") {
+    if (key.name === "c" || key.name === "с") {
         server.emit("SERVER:DISCONNECTED_ROOM", { pc: true, client_id: process.env.CLIENT_ID })
         process.exit()
     }
